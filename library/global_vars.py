7f7399ac15c8860cbec11002abc43e69579d263a
Truitt Savell <truitt.savell@gamebreaking.com>
2023-12-11 12:05:00 -0600

    feat: Refactor Snake to not be one giant file and instead use standard python best practices

---
+++ library/global_vars.py
@@ -0,0 +1,23 @@
+#Global Variables
+
+GRIDSIZE = 20
+
+GRID_WIDTH = 25
+GRID_HEIGHT = 25
+
+SCREEN_WIDTH = GRID_WIDTH * GRIDSIZE
+SCREEN_HEIGHT = GRID_HEIGHT * GRIDSIZE
+
+UP = (0,-1)
+DOWN = (0,1)
+LEFT = (-1,0)
+RIGHT = (1,0)
+
+FPS = 60
+
+T = TypeVar('T')
+GRIDLOCATION = Tuple[int, int]
+LOCATION = TypeVar('LOCATION')
+
+GRID = []
+MOVE_QUEUE = []
--- snake.py
+++ snake.py
@@ -7,31 +7,8 @@ import math
 import argparse
 import collections
 import heapq
+from Library.global_vars import *
 from typing import Protocol, Dict, List, Iterator, Tuple, TypeVar, Optional
-
-#Global Variables
-
-GRIDSIZE = 20
-
-GRID_WIDTH = 50
-GRID_HEIGHT = 24
-
-SCREEN_WIDTH = GRID_WIDTH * GRIDSIZE
-SCREEN_HEIGHT = GRID_HEIGHT * GRIDSIZE
-
-UP = (0,-1)
-DOWN = (0,1)
-LEFT = (-1,0)
-RIGHT = (1,0)
-
-FPS = 60
-
-T = TypeVar('T')
-GRIDLOCATION = Tuple[int, int]
-LOCATION = TypeVar('LOCATION')
-
-GRID = []
-MOVE_QUEUE = []
 
 class Graph(Protocol):
     def neighbors(self, id: LOCATION) -> List[LOCATION]: pass
